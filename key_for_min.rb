b1917dd39093bcebc729ae891f7e468011a09687
Tyler Huffman <ty.huff24@gmail.com>
2020-07-31 00:43:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest_value = 999999999999999999999999999
+namehash.each do |name, value|
+  if value < lowest_value
+    lowest_value = value
+  end
+end
+  namehash.find do |name, value|
+    value == lowest_value
+    return name
 end
